2466b4b5ae2d18e87cde3cf447d3feae42cf939d
jacquarg <guillaume.jacquart@hoodbrains.com>
2017-04-11 18:54:50 +0200

    Migrate to V3 format. Fetch and create document poc-ed :)

---
+++ models/consumption_statement.js
@@ -0,0 +1,23 @@
+const baseModel = require('../lib/base_model')
+
+module.exports = baseModel.createNew({
+  displayName: 'ConsumptionStatement',
+  name: 'org.fing.mesinfos.consumptionstatement',
+
+  // fields :
+    // contractNumber: String # Contract linked to this consumption
+    // billNumber: String # bill linked to this consumption.
+    // start: String # start date of the statement period.
+    // end: String # end date of the statement period.
+    // value: Number # Consumption value.
+    // statementType: String # Readed, estimated, ...
+    // statementCategory: String # Statemet subcategory
+    // statementReason: String # Statemet subcategory
+    // period: String # Simple designation of the temporal period of statement.
+    // cost: Number # Cost
+    // costsByCategory: Object # Details on costs
+    // valuesByCatergory: Object # Details on values
+    // similarHomes: Object # Similar home consumption comparisons.
+    // statements: [Object] # List of statement occured in this period.
+    // docTypeVersion: String
+})
